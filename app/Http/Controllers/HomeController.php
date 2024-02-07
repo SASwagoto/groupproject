50adfda6e44e7344fa26028451f647fd8f428f5d
Swagoto <saswagoto@gmail.com>
2024-02-07 12:32:15 +0600

    ok

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,9 @@ class HomeController extends Controller
     {
         return view('frontend.index');
     }
+
+    public function lawyer_profile()
+    {
+        return view('frontend.lawyer_profile');
+    }
 }
