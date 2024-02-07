221315a8cb4463a9177ae8117bbcd58e608cdafc
Swagoto <saswagoto@gmail.com>
2024-02-07 12:27:48 +0600

    ok

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,16 +1,30 @@
-<footer class="bg-white py-4 px-20 border-t-2">
-    <div class="w-1/2 flex justify-between">
-        <div class="footer_logo">
+<footer class="bg-white w-full py-4 px-20 border-t-2">
+    <div class="w-full flex justify-between">
+        <div class="footer_logo w-1/4">
             <img class="w-60" src="{{asset('assets/images/Logo_black.png')}}" alt="">
         </div>
-        <div class="menu flex flex-col gap-2">
-            <div class="w-full flex justify-between border-b">
+        <div class="menu w-full">
+            <div class="flex justify-between border-b">
                 <a href="#">Legal Services</a>
                 <a href="#">Legal Services</a>
                 <a href="#">Legal Services</a>
             </div>
-            <div class=""></div>
-            <div class=""></div>
+            <div class="flex justify-between border-b">
+                <a href="#">Know your Rights</a>
+                <a href="#">Know your Rights</a>
+                <a href="#">Know your Rights</a>
+                <a href="#">Know your Rights</a>
+                <a href="#">Know your Rights</a>
+                <a href="#">Know your Rights</a>
+            </div>
+            <div class="flex justify-between border-b">
+                <a href="#">Know your Rights</a>
+                <a href="#">Know your Rights</a>
+                <a href="#">Know your Rights</a>
+                <a href="#">Know your Rights</a>
+                <a href="#">Know your Rights</a>
+                <a href="#">Know your Rights</a>
+            </div>
         </div>
     </div>
 </footer>
