475673113e8c198740ced84559dbc0e0fed2f8c0
hasanmmr <mahmuduldmdhasan@gmail.com>
2024-02-07 12:25:47 +0600

    ok

--- resources/views/lawer/addlawer.blade.php
+++ resources/views/lawer/addlawer.blade.php
@@ -1,10 +1,11 @@
 <x-app-layout>
+<div>
 <form>
   <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname"><br>
+  <input class="w-[400px] rounded-md" type="text" id="fname" name="fname"><br>
 
-  <label for="ltype">Type:</label><br>
-  <select >
+  <labelclass="w-[400px]" for="ltype">Type:</label><br>
+  <select class="w-[400px] rounded-md" >
   <option>Criminal lawer</option>
   <option>Criminal lawer</option>
   <option>Criminal lawer</option>
@@ -12,21 +13,21 @@
   </select><br>
 
   <label for="lfirm">Law firm:</label><br>
-  <input type="text" id="lfirm" name="lfirm"><br>
+  <input class="w-[400px] rounded-md " type="text" id="lfirm" name="lfirm"><br>
 
   <label for="lnumber">Number:</label><br>
-  <input type="text" id="lnumber" name="lnumber"><br>
+  <input class="w-[400px] rounded-md" type="text" id="lnumber" name="lnumber"><br>
 
   <label for="lemail">Email:</label><br>
-  <input type="text" id="lemail" name="lemail"><br>
+  <input class="w-[400px] rounded-md" type="text" id="lemail" name="lemail"><br>
 
   <label for="laddress">Address:</label><br>
-  <input type="text" id="laddress" name="laddress"><br>
+  <input class="w-[400px] rounded-md" type="text" id="laddress" name="laddress"><br>
 
   <label for="lmessage">Message:</label><br>
-  <input type="text" id="lmessage" name="lmessage"><br> <br>
+  <input class="w-[400px] rounded-md" type="text" id="lmessage" name="lmessage"><br> <br>
 
   <button type="submit" class="w-[150px] py-3 px-2 bg-gradient-to-r from-[#FFA06F] to-[#FF5143] uppercase font-brand font-semibold text-white">submit</button><br>
 </form>
-
+</div>
 </x-app-layout>
